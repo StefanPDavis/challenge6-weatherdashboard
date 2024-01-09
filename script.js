5f32f344ef4957f433ec5e8bc2f649df1362f872
StefanPDavis <snoodle22@hotmail.com>
2024-01-08 22:01:24 -0800

    deleted function, added a bunch of variables in js, added some id's in html

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-function addResult(){
+const APIKey = "7b51adeaefa46b13afad2d8fa4f7189e"
 
-  inputCity = document.getElementById("enterCity").value;  
-  historyList = getInfo();
-  var searchCity =$("<div>") 
-  searchCity.attr('id',inputCity) 
-  searchCity.text(inputCity) 
-  searchCity.addClass("h4")
+var cityEl = document.getElementById("myInput").value
+var geoUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityEl + "&limit=5&appid=" + APIKey
 
-  
-  if (historyList.includes(inputCity) === false){
-      $(".history").append(searchCity)
-  }
-  $(".subtitle").attr("style","display:inline")
-  addInfo(inputCity);
-  
-}; 
+var geoLon;   
+var geoLat;
+var dataUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + geoLat + "&lon="+ geoLon + "&units=imperial&appid=" + APIKey
+
+var searchEl = document.getElementById("searchBtn")
+var historyEl = document.getElementById("history")
+var fiveDayEl = document.getElementById("fiveDay")
+var currentTempEl = document.getElementById("temperature");
+var currentHumidityEl = document.getElementById("humidity");
+var currentWindEl = document.getElementById("windSpeed");
+var currentUVEl = document.getElementById("UV-index");
